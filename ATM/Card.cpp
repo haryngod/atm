228f45e4fc1cb0b353141e9fb66ed907cded673e
haryngod <haryngod@gmail.com>
2022-08-11 19:30:13 +0900

    edit card class - add get card digital number method - add set account list to add list from account vector

--- ATM/Card.cpp
+++ ATM/Card.cpp
@@ -1,18 +1,18 @@
 #include "Card.h"
 
-Card::Card() { }
+Card::Card():
+	card_number(""), pin_number(-1), customer_name("")
+{ }
 
-bool Card::verifyCard(string number)
-{
-	if (number == card_number)
-	{
-		return true;
-	}
-	return false;
-}
+
+Card::Card(string cnum, int pnum, string cname):
+	card_number(cnum), pin_number(pnum), customer_name(cname)
+{ }
 
 bool Card::checkPinNumber(int pin)
 {
+	// Todo: pin number should check with bank api like as below
+	// return bank_api.verifyPinNumber(card_number, pin);
 	if (pin == pin_number)
 	{
 		return true;
@@ -23,7 +23,7 @@ bool Card::checkPinNumber(int pin)
 unordered_map<string, string> Card::getAccountList()
 {
 	unordered_map<string, string> map_account;
-	for (auto ac : account)
+	for (auto ac : account_list)
 	{
 		map_account.insert(pair<string, string>(ac.first, ac.second.getName()));
 	}
@@ -32,6 +32,23 @@ unordered_map<string, string> Card::getAccountList()
 
 Account Card::selectAccount(string number)
 {
-	return account[number];
+	if(account_list.find(number) != account_list.end())
+		return account_list[number];
+	return Account();
 }
 
+vector<int> Card::getCardDigitNumber()
+{
+	vector<int> digit_number;
+	for (auto i : card_number)
+		digit_number.push_back(i - '0');
+	return digit_number;
+}
+
+void Card::setAccountList(vector<Account> ac_list)
+{
+	for (auto ac : ac_list)
+	{
+		account_list.insert(pair<string, Account>(ac.getName(), ac));
+	}
+}
--- ATM/Card.h
+++ ATM/Card.h
@@ -8,15 +8,44 @@ using namespace std;
 class Card{
 public:
 	Card();
+	Card(string cnum, int pnum, string cname);
 private:
 	string card_number;
+
+	// Note: pin number just for test
 	int pin_number;
+
 	string customer_name;
 	// account number, account mapping
-	unordered_map<string, Account> account;
+	unordered_map<string, Account> account_list;
 public:
-	bool verifyCard(string number);
+	/// <summary>
+	/// Todo: connect bank API and check pin number there
+	/// check pin number
+	/// </summary>
+	/// <returns>if it is incorrect false</returns>
 	bool checkPinNumber(int pin);
+
+	/// <summary>
+	/// get account list
+	/// </summary>
+	/// <returns> account number and name</returns>
 	unordered_map<string, string> getAccountList();
+
+	/// <summary>
+	/// select account
+	/// </summary>
+	/// <returns> account object </returns>
 	Account selectAccount(string number);
+
+	/// <summary>
+	/// set account list
+	/// </summary>
+	void setAccountList(vector<Account> ac_list);
+
+	/// <summary>
+	/// get card number as digit number
+	/// </summary>
+	/// <returns> digit number as vector of integer </returns>
+	vector<int> getCardDigitNumber();
 };
